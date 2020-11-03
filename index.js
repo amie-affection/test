22f3f816434ed7760af505a2af5266813bf4b7cc
amie-affection <amie.affection@gmail.com>
2020-11-03 16:28:06 +0200

    done

---
+++ index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const burgerMenu = document.querySelector(".page-header__burgerMenu");
+const burgerLine1 = document.querySelector(".page-header__burgerLine1");
+const burgerLine2 = document.querySelector(".page-header__burgerLine2");
+const burgerLine3 = document.querySelector(".page-header__burgerLine3");
+
+console.log(burgerLine1);
+console.log(burgerLine2);
+console.log(burgerLine3);
+
+const clickHandler = (e) => {
+  burgerLine1.classList.toggle("page-header__burgerLine1_active");
+  burgerLine1.classList.toggle("page-header__burgerLine1");
+
+  burgerLine2.classList.toggle("page-header__burgerLine2_active");
+  burgerLine2.classList.toggle("page-header__burgerLine2");
+
+  burgerLine3.classList.toggle("page-header__burgerLine3_active");
+  burgerLine3.classList.toggle("page-header__burgerLine3");
+};
+
+burgerMenu.addEventListener("click", clickHandler);
+
+const mainPage = document.querySelector(".img-wrapper");
+const photo = document.querySelector(".main-page__front");
+const video = document.querySelector(".main-page__back");
+
+console.log(video);
+
+const showVideo = () => {
+  mainPage.classList.toggle("toggle");
+  video.play();
+};
+
+photo.addEventListener("click", showVideo);
